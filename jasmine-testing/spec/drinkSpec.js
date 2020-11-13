d30854e734179a8346f69c653ca05f1c3dd251d8
elomasney <eimearlomasney01@gmail.com>
2020-11-13 12:32:25 +0000

    Challenge 1 complete

--- jasmine-testing/spec/drinkSpec.js
+++ jasmine-testing/spec/drinkSpec.js
@@ -10,28 +10,28 @@ describe("checks age", function(){
  });
 
 it ("should return Drink Toddy when age is less than 14", function(){
-    var result = whatCanIDrink(13)
-    expect(whatCanIDrink(13)).toBe("Drink Toddy");
+    var result = whatCanIDrink(13);
+    expect(result).toBe("Drink Toddy");
 });
 
 it ("should return Drink Coke when age is less than 18", function(){
-    var result= whatCanIDrink(17)
-    expect(whatCanIDrink(17)).toBe("Drink Coke");
+    var result= whatCanIDrink(17);
+    expect(result).toBe("Drink Coke");
 });
 
 it ("Should return Drink Beer when age is less than 21", function(){
-    var result = whatCanIDrink(20)
-    expect(whatCanIDrink(20)).toBe("Drink Beer");
+    var result = whatCanIDrink(20);
+    expect(result).toBe("Drink Beer");
 });
 
 it ("should return Drink Whisky if age is less than 130", function(){
-    var result = whatCanIDrink(129)
-    expect(whatCanIDrink(129)).toBe("Drink Whisky");
+    var result = whatCanIDrink(129);
+    expect(result).toBe("Drink Whisky");
 });
 
 it ("should not be able to return a drink if age is greater than 130", function(){
-    var result = whatCanIDrink(131)
-    expect(whatCanIDrink(131)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+    var result = whatCanIDrink(131);
+    expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
 });
 });
 });
